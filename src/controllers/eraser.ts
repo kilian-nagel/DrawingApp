b0e6b76e21c66d2756a89189ad72d79096f43866
kilian-nagel <nagelkilian05@gmail.com>
2022-12-27 23:21:15 +0100

    refactor: improve controllers code quality and readability

--- src/controllers/eraser.ts
+++ src/controllers/eraser.ts
@@ -1,13 +1,9 @@
+
 import React from "react";
-import { parsePixel } from "./board";
-import { pencilColor, setPencilColor } from "./color-picker";
+import { handlePencilClickEvent } from "./pencil";
 
-let eraser_color = "FFFFFF";
+export let eraserColor = "FFFFFF";
 
-export function handleEraserClick(e:React.MouseEvent){
-    // Store the pencil_color to be able to go draw with the previous color once we're done using the eraser.
-    const old_pencil_color = pencilColor;
-    setPencilColor(eraser_color);
-    parsePixel(e);
-    setPencilColor(old_pencil_color);
+export function handleEraserClickEvent(e:React.MouseEvent,bool:boolean){
+    handlePencilClickEvent(e,bool);
 }
